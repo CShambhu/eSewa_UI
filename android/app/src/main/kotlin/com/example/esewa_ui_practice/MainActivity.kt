a723926ed5e96cf4463d75e8dbf9dbf7821abae6
chams <chaudharyshambhu388@gmail.com>
2026-05-16 23:58:02 +0545

    login page

---
+++ android/app/src/main/kotlin/com/example/esewa_ui_practice/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.esewa_ui_practice
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/login_email.dart
@@ -0,0 +1,323 @@
+import 'package:esewa_ui_practice/home.dart';
+import 'package:esewa_ui_practice/login_mobile.dart';
+import 'package:flutter/material.dart';
+
+class LoginEmail extends StatefulWidget {
+  const LoginEmail({super.key});
+
+  @override
+  State<LoginEmail> createState() => _LoginEmailState();
+}
+
+class _LoginEmailState extends State<LoginEmail> {
+  bool _checkRemember = true;
+  final TextEditingController emailcontroller = TextEditingController();
+  final TextEditingController pincontroller = TextEditingController();
+  final _formkey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 30, top: 60, right: 30),
+              child: Row(
+                children: [
+                  Image.asset("assets/images/esewa.png", height: 30),
+                  SizedBox(width: 5),
+                  Text(
+                    "Sewa",
+                    style: TextStyle(fontSize: 45, color: Colors.white),
+                  ),
+                  Spacer(),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: const Color.fromARGB(255, 49, 64, 71),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.close, size: 35, color: Colors.green),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 30, right: 30),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Welcome",
+                    style: TextStyle(fontSize: 40, color: Colors.white),
+                  ),
+                  Text(
+                    "Sign in with",
+                    style: TextStyle(fontSize: 20, color: Colors.white),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 25),
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20),
+                  topRight: Radius.circular(20),
+                ),
+              ),
+              child: Card(
+                margin: EdgeInsets.all(0),
+                color: Color.fromARGB(255, 49, 64, 71),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadiusGeometry.only(
+                    topLeft: Radius.circular(30),
+                    topRight: Radius.circular(30),
+                  ),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 18, top: 30, right: 18),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          TextButton(
+                            onPressed: () {
+                              Navigator.pushReplacement(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => LoginMobile(),
+                                ),
+                              );
+                            },
+                            child: Text(
+                              "Mobile Number",
+                              style: TextStyle(
+                                fontSize: 18,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                          TextButton(
+                            onPressed: () {},
+                            child: Text(
+                              "Email Address",
+                              style: TextStyle(
+                                fontSize: 18,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Divider(),
+
+                      SizedBox(height: 20),
+
+                      Form(
+                        key: _formkey,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "eSewa ID",
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: Colors.white,
+                              ),
+                            ),
+                            TextFormField(
+                              controller: emailcontroller,
+                              keyboardType: TextInputType.emailAddress,
+
+                              autovalidateMode:
+                                  AutovalidateMode.onUserInteractionIfError,
+                              decoration: InputDecoration(
+                                filled: true,
+                                fillColor: Color.fromARGB(255, 62, 81, 90),
+                                hintText: "Email Address",
+                                hintStyle: TextStyle(color: Colors.white),
+                              ),
+                              validator: (value) {
+                                if (value == null || value.isEmpty) {
+                                  return "please enter email";
+                                }
+                                final emailRegex = RegExp(
+                                  r'^[^@]+@[^@]+\.[^@]+',
+                                );
+                                if (!emailRegex.hasMatch(value)) {
+                                  return 'Please enter a valid email address';
+                                }
+                                return null;
+                              },
+                            ),
+                            SizedBox(height: 20),
+                            Text(
+                              "MPIN/Password",
+                              style: TextStyle(
+                                fontSize: 17,
+                                color: Colors.white,
+                              ),
+                            ),
+                            TextFormField(
+                              autovalidateMode:
+                                  AutovalidateMode.onUserInteractionIfError,
+                              decoration: InputDecoration(
+                                filled: true,
+                                fillColor: Color.fromARGB(255, 62, 81, 90),
+                                suffixIcon: IconButton(
+                                  onPressed: () {},
+                                  icon: Icon(
+                                    Icons.visibility,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                hintText: "Your 4-digit MPIN/Password",
+                                hintStyle: TextStyle(color: Colors.white),
+                              ),
+                              validator: (value) {
+                                if (value == null ||
+                                    value.isEmpty ||
+                                    value.length != 4) {
+                                  return "invalid number";
+                                }
+                                return null;
+                              },
+                            ),
+                            SizedBox(height: 20),
+
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Row(
+                                  children: [
+                                    Checkbox(
+                                      value: _checkRemember,
+                                      onChanged: (value) {
+                                        setState(() {
+                                          _checkRemember = value!;
+                                        });
+                                      },
+                                    ),
+                                    Text(
+                                      "Remember me",
+                                      style: TextStyle(color: Colors.white),
+                                    ),
+                                  ],
+                                ),
+                                Text(
+                                  "Forgot MPIN?",
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                              ],
+                            ),
+
+                            ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                minimumSize: Size(350, 50),
+                                backgroundColor: Colors.green,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                              ),
+                              onPressed: () {
+                                if (_formkey.currentState!.validate()) {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => Home(),
+                                    ),
+                                  );
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                    SnackBar(
+                                      content: Text("Login Successfull"),
+                                    ),
+                                  );
+                                }
+                              },
+                              child: Text(
+                                "Login",
+                                style: TextStyle(
+                                  fontSize: 17,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 20),
+
+                            Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(15),
+                                border: Border.all(
+                                  color: Colors.white,
+                                  width: 1,
+                                ),
+                              ),
+
+                              child: Padding(
+                                padding: const EdgeInsets.all(5.0),
+                                child: Row(
+                                  children: [
+                                    Icon(
+                                      Icons.phone_in_talk_outlined,
+                                      color: Colors.white,
+                                    ),
+                                    SizedBox(width: 5),
+
+                                    Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          "24*7 Help & Support",
+                                          style: TextStyle(
+                                            fontSize: 13,
+                                            color: Colors.white,
+                                          ),
+                                        ),
+                                        Text(
+                                          style: TextStyle(
+                                            fontSize: 13,
+                                            color: Colors.white,
+                                          ),
+                                          "Get quick solutions for eSewa-related queries.",
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 20),
+
+                            Center(
+                              child: Text(
+                                "Register",
+                                style: TextStyle(
+                                  fontSize: 17,
+                                  color: Colors.green,
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 20),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
